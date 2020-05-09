d193d434d60b4ae6dbe6026176ae58a035ad33fa
golubova <47048883+golubova@users.noreply.github.com>
2020-05-09 16:36:54 -0400

    Update content-comment.js

--- content-comment.js
+++ content-comment.js
@@ -6,5 +6,20 @@
 // IIFE format, prevent any variables from being accessed
 // by the global scope
 (function() {
+    const serverResponse = {{"success": true,    
+                             "data": [
+                                        {
+                                            "_id": "5eb6fc83cc14aea5b6ad6cb3",
+                                            "timestamp": "2020-05-09T18:54:59.741Z",
+                                            "username": "sean",
+                                            "url": "https://medium.com/@gilfink/building-a-chrome-extension-using-react-c5bfe45aaf36",
+                                            "highlighted_text": "The first thing you will want to do is to create the extension project",
+                                            "comment": "some comment",
+                                            "selector": "#6c33",
+                                            "replies": []         
+                                        }     
+                                     ] 
+                              };
+    const comment.document.querySelector(serverResponse.data[0].selector);
     console.log("Content script running...");
 })();
