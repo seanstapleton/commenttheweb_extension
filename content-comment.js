18540cc010fe462d73ddf91ba312f7cb1166e132
Sean Stapleton <seanstapleton100@gmail.com>
2020-05-09 15:57:21 -0400

    added manifest and test content script

---
+++ content-comment.js
@@ -0,0 +1,10 @@
+/*
+ * This file will be injected into the Medium article we are viewing.
+ * It will identify the sections of the article that need to be highlighted
+ */
+
+// IIFE format, prevent any variables from being accessed
+// by the global scope
+(function() {
+    console.log("Content script running...");
+})();
